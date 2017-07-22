6b30585ff5075971f762638a259b0c91786d6867
Rose Espiritu <rose.espiritu1@gmail.com>
2017-07-22 11:46:58 -0500

    [feat] adding to group table

--- src/hooks/getUserById.js
+++ src/hooks/getUserById.js
@@ -2,7 +2,7 @@ module.exports = function () { // eslint-disable-line no-unused-vars
   return function (hook) {
     const id = hook.data.userId;
     hook.app.service('users').get(id).then(user => {
-      // console.log(user, 'user data'),
+      console.log(user, 'user data'),
       hook.result.result = user, 
     });
     return Promise.resolve(hook);
--- src/hooks/getUserData.js
+++ src/hooks/getUserData.js
@@ -1,10 +1,12 @@
 module.exports = function (options = {}) { // eslint-disable-line no-unused-vars
   return function (hook) {
-    const newUser = hook.data.username;
-    return hook.app.service('users').find({ query: { username: newUser } }).then((val) =>{   
-      console.log(`new user ${val.data[0].username} is added to the ${hook.data.packId} pack `);
-      hook.data.userId = val.data[0].id;
-      return hook;
-    });
+    if (!hook.data.userId) {
+      const newUser = hook.data.username;
+      return hook.app.service('users').find({ query: { username: newUser } }).then((val) =>{   
+        console.log(`new user ${val.data[0].username} is added to the ${hook.data.packId} pack `);
+        hook.data.userId = val.data[0].id;
+        return hook;
+      });
+    }
   };
 };
